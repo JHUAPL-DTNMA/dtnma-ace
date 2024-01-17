121bea717e2064ed9cfc7138c3cee14243a51b85
Brian Sipos <brian.sipos@jhuapl.edu>
2024-01-17 12:11:50 -0500

    Replacing manual cache action with test environment change

--- src/ace/adm_set.py
+++ src/ace/adm_set.py
@@ -85,7 +85,7 @@ class AdmSet:
         If False, the cache is kept in-memory.
     '''
 
-    def __init__(self, cache_dir:str=None, clean_cache=False):
+    def __init__(self, cache_dir:str=None):
         if cache_dir is False:
             self.cache_path = None
         else:
@@ -106,7 +106,7 @@ class AdmSet:
             self._db_sess.commit()
             db_vers = cur_vers
 
-        if db_vers != cur_vers or clean_cache == True:
+        if db_vers != cur_vers:
             LOGGER.info(
                 'Recreating cache DB version %s because of old version %s',
                 cur_vers, db_vers
